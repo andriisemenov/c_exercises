b98a75e31921493115e66c6063f36cc8bf99d433
Andrii Semenov <andrewhind@yandex.ua>
2021-01-17 17:44:09 +0100

    Advanced C course on Linkedin

--- advanced_c_course/03-02_challenge.c
+++ advanced_c_course/03-02_challenge.c
@@ -5,5 +5,5 @@ int main()
     int number;
     printf("Enter an integer\n");
     scanf("%d", &number);
-    printf("The floating point version is: %.1f", (float)number);
+    printf("The floating point version is: %.1f\n", (float)number);
 }
---
+++ advanced_c_course/03-06_challenge.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/*  A program generating primes and showcasing pointer returning function
+*   The key is to define a pointer variable of a type used in the array
+*   and then to assign the result of the pointer returning function to this variable(pointer)*/
+
+#define PRIMES_NUM 5
+
+int *getPrimes(void)
+{
+    static int primes[PRIMES_NUM];
+    int count, i = 3, c, index = 0;
+    primes[index] = 2;
+    index++;
+    for (count = 2; count <= sizeof(primes) / sizeof(int);)
+    {
+        for (c = 2; c <= i - 1; c++)
+        {
+            if (i % c == 0)
+                break;
+        }
+        if (c == i)
+        {
+            primes[index] = i;
+            index++;
+            count++;
+        }
+        i++;
+    }
+    return primes;
+}
+
+int main()
+{
+    printf("%d prime numbers: \n", PRIMES_NUM);
+    int *primes;
+    primes = getPrimes();
+    for (int i = 0; i < PRIMES_NUM; i++)
+    {
+        printf("%d ", primes[i]);
+    }
+    printf("\n");
+}
